a52ca0c05b9944dc3086158e301d2de8e5d5ac10
Ted Irland <irlandth@gmail.com>
2021-02-12 06:58:08 -0600

    Pushing w/ custom middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -8,11 +9,15 @@ dotenv.config()
 connectDB()
 const app = express()
 
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
 app.get('/', (req, res) => {
   res.send('API is running')
 })
-
-app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(
